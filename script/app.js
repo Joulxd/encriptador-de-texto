9bfd7f4059960c71f8421de25225a4ef64a3027d
Joulxd <jonathanquinteroxd@gmail.com>
2024-07-24 02:41:25 -0600

    añadiendo funcionalidad a desencriptar y copiar, ademas de algunas funciones menores para un uso mas intuitivo

--- script/app.js
+++ script/app.js
@@ -1,20 +1,42 @@
 let textoOriginal;
-let textoCodificado;
+let textoEncriptado;
+let textoDesencriptado;
 let caracteresACambiar = ['a','e','i','o','u'];
 let nuevosCaracteres = ["ai","enter","imes","ober","ufat"];
 
-function botonCodificar(){
+function botonEncriptar(){
     textoOriginal = obtenerValor('areaDeTexto');
     console.log(textoOriginal, typeof(textoOriginal));
     if(verificarMinusculas(textoOriginal)){
         volverInvisibleElementoHTML('advertenciaMinusculas');
-        desplegarEncriptacion(codificar(textoOriginal))
+        desplegarEncriptacion(encriptar(textoOriginal));
+    }else{
+        asignarValorElementoHTML('areaDeTexto','');
+        volverVisibleElementoHTML('advertenciaMinusculas');
+        insertarTextoElementoHTML('textoResultado','');
+    }
+    
+}
+
+function botonDesencriptar(){
+    textoEncriptado = obtenerValor('areaDeTexto');
+    if(verificarMinusculas(textoEncriptado)){
+        volverInvisibleElementoHTML('advertenciaMinusculas');
+        desplegarEncriptacion(desencriptar(textoEncriptado));
     }else{
         asignarValorElementoHTML('areaDeTexto','');
         volverVisibleElementoHTML('advertenciaMinusculas');
     }
-    
 }
+
+function copiarTexto() {
+    navigator.clipboard.writeText(document.getElementById('textoResultado').innerText).then(function() {
+    }).catch(function(error) {
+        console.error('Error al copiar el texto: ', error);
+    });
+    asignarValorElementoHTML('areaDeTexto','');
+}
+
 
 function asignarValorElementoHTML(elemento,texto){
     document.getElementById(elemento).value = texto;
@@ -36,17 +58,47 @@ function obtenerValor(elemento){
     return document.getElementById(elemento).value;
 }
 
-function codificar(textoOriginal){
+function encriptar(textoOriginal){
 
-    textoCodificado = "";
+    textoEncriptado = "";
     for (let i = 0; i < textoOriginal.length; i++) {
         if(caracteresACambiar.includes(textoOriginal[i])){
-            textoCodificado = textoCodificado+nuevosCaracteres[caracteresACambiar.indexOf(textoOriginal[i])];
+            textoEncriptado = textoEncriptado+nuevosCaracteres[caracteresACambiar.indexOf(textoOriginal[i])];
         }else{
-            textoCodificado = textoCodificado+textoOriginal[i];
+            textoEncriptado = textoEncriptado+textoOriginal[i];
         }
     }
-    return textoCodificado;
+    return textoEncriptado;
+}
+
+
+function desencriptar(textoEncriptado){
+    textoDesencriptado="";
+
+  for (let i = 0; i < textoEncriptado.length; i++) {
+      // Verificar si la secuencia actual es una de las posibles secuencias encriptadas
+      if (textoEncriptado.substring(i, i + 5) === 'enter') {
+          textoDesencriptado += 'e';
+          i += 4; // Avanzar el índice para saltar el texto encriptado
+      } else if (textoEncriptado.substring(i, i + 4) === 'imes') {
+          textoDesencriptado += 'i';
+          i += 3; // Avanzar el índice para saltar el texto encriptado
+      } else if (textoEncriptado.substring(i, i + 2) === 'ai') {
+          textoDesencriptado += 'a';
+          i += 1; // Avanzar el índice para saltar el texto encriptado
+      } else if (textoEncriptado.substring(i, i + 4) === 'ober') {
+          textoDesencriptado += 'o';
+          i += 3; // Avanzar el índice para saltar el texto encriptado
+      } else if (textoEncriptado.substring(i, i + 4) === 'ufat') {
+          textoDesencriptado += 'u';
+          i += 3; // Avanzar el índice para saltar el texto encriptado
+      } else {
+          // Si no es una secuencia encriptada, agregar el carácter tal cual
+          textoDesencriptado += textoEncriptado[i];
+      }
+  }
+
+  return textoDesencriptado;
 }
 
 function verificarMinusculas(textoOriginal){
@@ -71,6 +123,7 @@ function alinearTexto(elemento, alineacion){
 }
 
 function desplegarEncriptacion(textoCodificado){
+    insertarTextoElementoHTML('textoResultado','');
     volverInvisibleElementoHTML('imagenResultado');
     volverInvisibleElementoHTML('tituloNingunMensaje');
     volverInvisibleElementoHTML('ingresaTexto');
